ebd1b1f9b9b29ff203ec22e19cc304efc375ae5e
Greg Wolff <gwolff@blues.com>
2021-01-07 17:29:49 +0000

    rounded out basic serial bus transactions

--- notecard/bus-transactor.js
+++ notecard/bus-transactor.js
@@ -1,25 +1,89 @@
 
 
 async function queryAvailableBytes(rw){
-    const requestBuffer = Buffer.from([0, 0]);
+    const buffer = Buffer.from([0, 0]);
     const QUERY_REQUEST_SIZE = 2;
-    var numBytesWritten = await rw.write(QUERY_REQUEST_SIZE, requestBuffer);
+    const QUERY_RESPONSE_SIZE = 2;
+
+    var numBytesWritten = await rw.write(QUERY_REQUEST_SIZE, buffer);
     if(numBytesWritten < QUERY_REQUEST_SIZE ){
         
         throw new Error("this was too small");
     }
-    throw new Error("more shiiiii.... stuff to do");
+    
+    var numBytes = await rw.read(QUERY_RESPONSE_SIZE, buffer);
+    if(numBytes < QUERY_RESPONSE_SIZE){
+        throw new Error("number of response bytes is fewer than 2");
+    }
+
+    if(buffer[1] != 0){
+        throw new Error("response payload size on query is non-zero");
+    }
+
+    return buffer[0];
 
 
 }
 
 
-async function sendRequest(readwriter, request){
+
+async function sendRequest(rw, request, payloadSize, token={isCancelled:false}){
+    const REQUEST_FRAME_HEADER_SIZE = 1;
+    const chunkSize = payloadSize + REQUEST_FRAME_HEADER_SIZE;
+    
+    const buffer = Buffer.alloc(chunkSize);
+    
+    var requestSlice = request.slice(0);
+
+    
+    var bytesToSend = requestSlice.length;
+    while(bytesToSend > 0 && token.isCancelled === false){
+        //payloadsize = (payloadsize > maxPayloadSize) ? maxPayloadSize : payloadsize;
+        bytesToSend = (bytesToSend > payloadSize) ? payloadSize : bytesToSend;
+        requestSlice.copy(buffer, REQUEST_FRAME_HEADER_SIZE, 0, bytesToSend);
+        
+        
+        //numBytesToWrite = (numBytesToWrite > chunksize) ? chunksize : numBytesToWrite;
+
+        buffer[0] = bytesToSend;
+        var numBytesToWrite = bytesToSend + REQUEST_FRAME_HEADER_SIZE;
+        var numBytesWritten = await rw.write(numBytesToWrite, buffer);
+
+
+        requestSlice = requestSlice.slice(numBytesWritten - REQUEST_FRAME_HEADER_SIZE);
+        bytesToSend = requestSlice.length;
+    }
 
 }
 
-async function readResponse(readwriter){
+async function readResponse(rw, numBytesAvailable, payloadSize, token={isCancelled:false}){
+    const RESPONSE_FRAME_HEADER_SIZE = 2;
+    const chunkSize = payloadSize + RESPONSE_FRAME_HEADER_SIZE;
+    var readRequest = Buffer.from([0, 0]);
+    var readResponse = Buffer.alloc(chunkSize);
+    var response = Buffer.alloc(0);
 
+    while (numBytesAvailable > 0 && token.isCancelled === false){
+        var numBytesRequested = (numBytesAvailable > payloadSize) ? payloadSize : numBytesAvailable;
+        readRequest[1] = numBytesRequested;
+
+        var numBytesWritten = await rw.write(readRequest.length, readRequest);
+
+        
+        
+        
+    
+        var numBytesToRead = numBytesRequested + RESPONSE_FRAME_HEADER_SIZE;
+        //numBytesToRead = (numBytesRead > chunkSize) ? chunkSize : numBytesToRead;
+
+        var numBytesRead = await rw.read(numBytesToRead, readResponse);
+        numBytesAvailable = readResponse[0];
+        //numBytesSent = readResponse[1];
+
+        response = Buffer.concat([response, readResponse.slice(RESPONSE_FRAME_HEADER_SIZE, numBytesRead)])
+    }
+    
+    return response;
 }
 
 module.exports = {queryAvailableBytes, sendRequest, readResponse};
--- test/test.js
+++ test/test.js
@@ -12,8 +12,9 @@ function createReadWriter(){
         writeBufferIndex:0,
         read: async function (numBytes, outputBuffer){
             var availableBytes = this.readBuffer.length - this.readBufferIndex;
-            numBytes = (numBytes > availableBytes) ? availableBytes : numBytes;
             
+            numBytes = (numBytes > availableBytes) ? availableBytes : numBytes;
+
             this.readBuffer.copy(outputBuffer, 0, this.readBufferIndex, this.readBufferIndex + numBytes);
             this.readBufferIndex += numBytes
             
@@ -30,7 +31,7 @@ function createReadWriter(){
 
             return numBytes;
         },
-        reset: function(){
+        reset: function() {
             this.readBufferIndex = 0;
             this.writeBufferIndex = 0;
             this.readBuffer.fill(0);
@@ -41,12 +42,26 @@ function createReadWriter(){
     return rw;
 }
 
+function createPromise(handler){
+    var resolve, reject;
+  
+    var promise = new Promise(function(_resolve, _reject){
+      resolve = _resolve; 
+      reject = _reject;
+      if(handler) handler(resolve, reject);
+    })
+    
+    promise.resolve = resolve;
+    promise.reject = reject;
+    return promise;
+}
 
-describe('buffer readwriter', function() {
-    describe('read', function() {
+
+describe('buffer readwriter', () =>  {
+    describe('read', () =>  {
         var rw = createReadWriter();
         rw.readBuffer = Buffer.from("hello");
-        it('should read values in buffer', async function(){
+        it('should read values in buffer', async () => {
             var localBuffer = Buffer.alloc(255);
             var numBytesRead = await rw.read(5, localBuffer)
             assert.equal(numBytesRead, 5, "Expects to read 5 bytes");
@@ -70,10 +85,10 @@ describe('buffer readwriter', function() {
         });
     });
 
-    describe('write', function() {
+    describe('write', () =>  {
         var rw = createReadWriter();
         rw.writeBuffer = Buffer.alloc(255);
-        it('should write values to writer buffer', async function(){
+        it('should write values to writer buffer', async () => {
             var localBuffer = Buffer.from("hello");
             var numBytesWritten = await rw.write(localBuffer.length, localBuffer);
             assert.equal(numBytesWritten, localBuffer.length);
@@ -83,8 +98,8 @@ describe('buffer readwriter', function() {
 
 });
 
-describe('bus transactor', function() {
-    describe('getAvailableBytes', function() {
+describe('bus transactor', () =>  {
+    describe('getAvailableBytes', () =>  {
         var rw = createReadWriter();
         rw.writeBuffer = Buffer.alloc(255);
         rw.writeBuffer[0] = 99;
@@ -93,19 +108,266 @@ describe('bus transactor', function() {
 
         
 
-        it('should write [0,0] on to readwriter', async function() {
+        it('should write [0,0] on to readwriter', async () =>  {
             var numBytes = await transactor.queryAvailableBytes(rw);
             assert.equal(rw.writeBuffer[0], 0, "First byte in write buffer should be 0");
             assert.equal(rw.writeBuffer[1], 0, "Second byte in write buffer should be 0");
         });
 
-        it('should throw error if not enough bytes are written',  function (){
+        it('should throw error if not enough bytes are written',  async function (){
             rw.writeBufferIndex = rw.writeBuffer.length;
             var numBytes = 7
             //numBytes = ;
-            assert.rejects(async () => await transactor.queryAvailableBytes(rw));
+            await assert.rejects(async () => await transactor.queryAvailableBytes(rw));
 
         });
+
+        it('should return number bytes available to read - zero', async () =>  {
+            rw.reset();
+            var numBytes = await transactor.queryAvailableBytes(rw);
+            assert.equal(numBytes, 0, 'Expect 0 bytes, if first byte in read buffer is zero');
+        });
+
+        it('should return number of bytes available to read - non-zero', async () =>  {
+            rw.reset();
+            rw.readBuffer[0] = 13;
+
+            var numBytes = await transactor.queryAvailableBytes(rw);
+            assert.equal(numBytes, 13, 'Expect 13 bytes to be available to read');
+        });
+
+        it('should throw error if number of bytes in read payload is non-zero', async () => {
+            rw.reset();
+            rw.readBuffer[1] = 17;
+            await assert.rejects(async () => {await transactor.queryAvailableBytes(rw)},
+            (err) => {
+              assert.strictEqual(err.name, 'Error');
+              assert.strictEqual(err.message, 'response payload size on query is non-zero');
+              return true;
+            });
+        });
+
+        it('should throw error if not enough bytes were read', async () => {
+            rw.reset();
+            rw.readBufferIndex = rw.readBuffer.length;
+            await assert.rejects(async () => { await transactor.queryAvailableBytes(rw)}, 
+            (err) => {
+                assert.strictEqual(err.name, 'Error');
+                assert.strictEqual(err.message, 'number of response bytes is fewer than 2');
+                return true;
+            })
+        });
+
     
-  });
+    });
+
+    describe('sendRequest', () => {
+        var rw = createReadWriter();
+        rw.writeBuffer = Buffer.alloc(203);
+
+        it('should write number of payload bytes sent as first byte', async () => {
+            rw.reset();
+            var payload = Buffer.from("my_payload");
+            var payloadSize = payload.length;
+
+            await transactor.sendRequest(rw, payload, payloadSize);
+
+            assert.strictEqual(rw.writeBuffer[0], payload.length, 'Payload length not first byte in write buffer');
+        });
+
+        it('should write payload as single chunk if payload less than payload chunk size', async () => {
+            rw.reset();
+            var payload = Buffer.from("my_payload");
+            var payloadSize = payload.length;
+
+            await transactor.sendRequest(rw, payload, payloadSize);
+
+            assert.strictEqual(rw.writeBuffer.slice(1,payload.length + 1).toString(), "my_payload", 'Payload value not in write buffer');
+        });
+
+        it('should write payload as multiple chunks if payload greater than chunk size', async () => {
+            rw.reset();
+            const payload = Buffer.from("my_payload");
+            const PAYLOAD_REDUCTION = 3;
+            const payloadSize = payload.length - PAYLOAD_REDUCTION;
+
+            await transactor.sendRequest(rw, payload, payloadSize);
+
+            var payloadBoundary = payloadSize + 1;
+            assert.strictEqual(rw.writeBuffer[0], payloadSize, 'Initial chunk size is incorrect');
+            assert.strictEqual(rw.writeBuffer.slice(1,payloadBoundary).toString(), "my_payl", 'First payload chunk not in write buffer');
+            
+            var writeBufferChunk = rw.writeBuffer.slice(payloadBoundary);
+            assert.strictEqual(writeBufferChunk[0], PAYLOAD_REDUCTION, 'Second chunk size is incorrect');
+            assert.strictEqual(writeBufferChunk.slice(1, PAYLOAD_REDUCTION + 1).toString(), "oad", "Second payload chunk is not written correctly");
+
+        });
+
+        it('should cancel write if a cancel is requested during sentRequest operation', async () => {
+            var rwHoldWrite = createReadWriter();
+            rwHoldWrite.writeBuffer = Buffer.alloc(203);
+            
+
+            var p = createPromise((r,e) => {});
+
+            rwHoldWrite.write =  async function (numBytes, inputBuffer){
+                
+                await p;
+                var availableBytes = this.writeBuffer.length - this.writeBufferIndex;
+                numBytes = (numBytes > availableBytes) ? availableBytes : numBytes;
+    
+                inputBuffer.copy(this.writeBuffer, this.writeBufferIndex, 0, numBytes);
+                this.writeBufferIndex += numBytes;
+                
+                return numBytes;
+            };
+
+            var canceller = {
+                isCancelled: false,
+                onCancel: () => {},
+                cancel: () => {
+                    canceller.isCancelled = true;
+                    canceller.onCancel();
+                }
+            };
+
+
+            var sender = transactor.sendRequest(rwHoldWrite, Buffer.from("my_payload"), 1, canceller);
+            canceller.cancel();
+            p.resolve("closing wait promise");
+
+            await sender;
+
+            assert.strictEqual(rwHoldWrite.writeBuffer[4], 0, "Expect no additional values written if cancelled")
+
+        });
+
+    });
+
+    describe('readResponse', () => {
+        var rw = createReadWriter();
+        rw.writeBuffer = Buffer.alloc(203);
+        rw.readBuffer = Buffer.alloc(203);
+
+        it('should write 0 as the first byte to the write buffer on read request', async () => {
+            rw.reset();
+            rw.writeBuffer[0] = 99;
+            var numBytesAvailable = 1;
+            var payloadSize = 3;
+
+            await transactor.readResponse(rw, numBytesAvailable, payloadSize);
+
+            assert.strictEqual(rw.writeBuffer[0], 0, 'Expect first element of write buffer to be 0 on read request')
+        });
+
+        it('should write number of bytes requested as the second byte to the write buffer on read request', async () => {
+            rw.reset();
+            rw.writeBuffer[1] = 99;
+            var numBytesAvailable = 1;
+            var payloadSize = numBytesAvailable + 3;
+
+            await transactor.readResponse(rw, numBytesAvailable, payloadSize);
+
+            assert.strictEqual(rw.writeBuffer[1], 1, 'Expect second element of write buffer to be number of bytes requested for send')
+        });
+
+        it('should return payload from read buffer', async () => {
+            rw.reset();
+            var payload = Buffer.from("my_payload");
+            rw.readBuffer = Buffer.alloc(payload.length + 3);
+            rw.readBuffer[0] = 0;
+            rw.readBuffer[1] = payload.length;
+            payload.copy(rw.readBuffer, 2);
+
+            var numBytesAvailable = payload.length;
+            var payloadSize = numBytesAvailable + 3;
+
+            var response = await transactor.readResponse(rw, numBytesAvailable, payloadSize);
+
+            assert.strictEqual(response.toString(), payload.toString(), 'Response expected to contain read buffer payload');
+
+        });
+
+        it('should return payload from read buffer response is larger than chunk size', async () => {
+            rw.reset();
+            var payload = Buffer.from("my_payload");
+            var numBytesAvailable = payload.length;
+            var payloadSize = numBytesAvailable - 1;
+
+            // populate first chunk in read buffer
+            var bytesSent = payloadSize;
+            var remainingBytes = numBytesAvailable - bytesSent;
+            rw.readBuffer = Buffer.alloc(payload.length + 5);
+            rw.readBuffer[0] = remainingBytes;
+            rw.readBuffer[1] = bytesSent;
+            payload.copy(rw.readBuffer, 2);
+
+            
+            // populate second chunk in read buffer
+            bytesSent = payload.length - payloadSize;
+            remainingBytes = 0;
+
+            var index = payloadSize + 2;
+            var payloadSlice = payload.slice(payloadSize);
+
+            rw.readBuffer[index++] = remainingBytes;
+            rw.readBuffer[index++] = bytesSent;
+            payloadSlice.copy(rw.readBuffer, index);
+            
+
+            var response = await transactor.readResponse(rw, numBytesAvailable, payloadSize);
+            assert.strictEqual(rw.writeBufferIndex, 4, 'Write buffer not incremented to expected value');
+            assert.strictEqual(rw.writeBuffer[0], 0, 'Write buffer expects read request byte');
+            assert.strictEqual(rw.writeBuffer[1], payloadSize, 'Write buffer expects byte value of number of bytes to read in this request');
+            assert.strictEqual(rw.writeBuffer[2], 0, 'Write buffer expects read request byte');
+            assert.strictEqual(rw.writeBuffer[3], 1, 'Write buffer expects byte value of number of bytes to read in this request');
+            
+            assert.strictEqual(response.toString(), payload.toString(), 'Response expected to contain read buffer payload');
+
+        });
+
+        it('should cancel read  if a cancel is requested during readResponse operation', async () => {
+            var rwHoldWrite = createReadWriter();
+            rwHoldWrite.writeBuffer = Buffer.alloc(203);
+            rwHoldWrite.readBuffer = Buffer.from([2, 1, 65, 1, 1, 65, 0, 1, 65])
+            
+
+            var p = createPromise((r,e) => {});
+
+            rwHoldWrite.write =  async function (numBytes, inputBuffer){
+                
+                await p;
+                var availableBytes = this.writeBuffer.length - this.writeBufferIndex;
+                numBytes = (numBytes > availableBytes) ? availableBytes : numBytes;
+    
+                inputBuffer.copy(this.writeBuffer, this.writeBufferIndex, 0, numBytes);
+                this.writeBufferIndex += numBytes;
+                
+                return numBytes;
+            };
+
+            var canceller = {
+                isCancelled: false,
+                onCancel: () => {},
+                cancel: () => {
+                    canceller.isCancelled = true;
+                    canceller.onCancel();
+                }
+            };
+
+
+            var reader = transactor.readResponse(rwHoldWrite, 11, 1, canceller);
+            canceller.cancel();
+            p.resolve("closing wait promise");
+
+            await reader;
+
+            assert.strictEqual(rwHoldWrite.writeBufferIndex, 2, "Expect no additional read request bytes in write buffer if cancelled")
+        });
+
+    });
+  
 });
+
+
+
