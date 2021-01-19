59214e8052eac67560cda7da45eef7e5034b9cc2
Greg Wolff <gwolff@blues.com>
2021-01-19 16:09:04 +0000

    Gets started with including testing for Notecard node

---
+++ test/test_notecard_node.js
@@ -0,0 +1,7 @@
+
+const assert = require('assert');
+const helper = require("node-red-node-test-helper");
+const ncNode = require('../notecard/notecard-node.js');
+
+
+//helper.init(require.resolve('node-red'));
