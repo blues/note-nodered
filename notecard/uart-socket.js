abdc355b6c432a4458348e5e206a8d66ffffffe9
Greg Wolff <gwolff@blues.com>
2021-05-12 12:04:46 -0400

    Migrates UART transactor to socket

---
+++ notecard/uart-socket.js
@@ -0,0 +1,159 @@
+const SerialPort = require("serialport");
+const Readline = SerialPort.parsers.Readline
+
+const MAX_PAYLOAD_CHUNK_BYTES = 250;
+const MESSAGE_TERMINATOR = '\r\n';
+
+class UartSocket{
+
+    Port = "";
+    BaudRate = 9600;
+    _serial = null;
+    constructor(config){
+        if (typeof config !== 'object')
+            return
+
+        if('port' in config)
+            this.Port = config.port;
+
+        if('baudrate' in config)
+            this.BaudRate = config.baudrate;   
+    }
+
+    get IsOpen(){
+        if(this._serial === null)
+            return false;
+
+        return this._serial.isOpen;
+    }
+
+    
+    async Open(){
+
+        return new Promise((resolve, reject) => {
+            this._serial = new SerialPort(this.Port, {baudRate:this.BaudRate});
+            this._serial.on("open", err => {
+                if(err){
+                    reject(err);
+                    return
+                }
+                resolve();
+            })
+        })
+    }
+
+    async Close(){
+        return new Promise((resolve, reject) => {
+            if(this._serial === null){
+                resolve();
+                return;
+            }
+            
+            this._serial.close(err =>{
+                if(err){
+                    reject(err);
+                    return
+                }
+                resolve();
+            });
+        })
+    }
+
+    async SendReceive(request){
+        if(! this.IsOpen)
+            throw(new Error ('Socket not open'));
+
+        await sendDataInChunks(this._serial, request);
+        const response = await readline(this._serial);
+        return(response)
+
+    }
+
+    async Send(data){
+        if(! this.IsOpen)
+            throw(new Error ('Socket not open'));
+
+        await sendDataInChunks(this._serial, data);
+    }
+
+    
+
+}
+
+function FindPort(searchParams){
+
+    hasAllParams = (p) => {
+        for(f in searchParams){
+            if(!p.hasOwnProperty(f) || p[f] != searchParams[f])
+                    return false
+        }
+        return true;
+    };
+
+    parsePortList = (ports) =>{
+        let list = []
+        for (let i = 0; i < ports.length; i++){
+            const p = ports[i];
+            if(hasAllParams(p)){
+            
+                list.push(p.path)
+            }
+        }
+        return list;
+    }
+
+    return new Promise((resolve, reject) => {
+        SerialPort.list().then(
+            ports => {
+                resolve(parsePortList(ports))
+            },
+            err => reject(err)
+        )
+    });
+}
+
+module.exports = {UartSocket, FindPort};
+
+async function write(serial, data){
+    return new Promise((resolve, reject) => {
+        const success = serial.write(data);
+        if(!success){
+            reject(new Error("failed to write serial data"));
+            return
+        }
+        resolve();
+    });
+}
+
+async function drain(serial){
+    return new Promise((resolve, reject) => {
+        serial.drain(() => {
+            resolve();
+        });
+    });
+}
+
+async function readline(serial){
+    return new Promise((resolve, reject) => {
+        let parser = new Readline({ delimiter: MESSAGE_TERMINATOR });
+        serial.pipe(parser);
+        parser.on('data', (d) => resolve(d));
+    });
+
+}
+
+async function sendDataInChunks(serial, data){
+
+    let d = data.slice(0);
+    let n = d.length;
+    while(n > 0){
+        if(n > MAX_PAYLOAD_CHUNK_BYTES)
+            n = MAX_PAYLOAD_CHUNK_BYTES;
+
+        await write(serial, d.slice(0, n));
+        await drain(serial);
+        d = d.slice(n);
+        n = d.length;
+    }
+    
+}
---
+++ test/test_uart_socket.js
@@ -0,0 +1,232 @@
+
+
+var assert = require('assert');
+const should = require('should');
+const uart = require('../notecard/uart-socket.js');
+
+var port = "";
+
+findNotecard().then((p) => {
+    port = p;});
+
+context('UART', () => {
+    describe('UART Socket', () => {
+        describe('constructor', () => {
+
+            it('should have default config values', () => {
+                const s = new uart.UartSocket();
+                assert.strictEqual(s.Port,"");
+                assert.strictEqual(s.BaudRate, 9600);
+                console.log(port)
+
+            });
+
+            it('should have correct port when passed in config object', () => {
+                const c = {port: "portABC"};
+                const s = new uart.UartSocket(c);
+
+                assert.strictEqual(s.Port, c.port);
+            });
+
+            it('should have correct baud rate when passed in config object', () => {
+                const c = {baudrate: 7};
+                const s = new uart.UartSocket(c);
+
+                assert.strictEqual(s.BaudRate, c.baudrate);
+            });
+        });
+
+        
+
+        describe('Open, IsOpen, and Close', () => {
+            it('should open the port and \'IsOpen\' should be true, close the port', async () => {
+                const s = new uart.UartSocket({port:port})
+                await s.Open();
+                const tfOpen = s.IsOpen;
+                await s.Close();
+                const tfClose = s.IsOpen;
+
+                tfOpen.should.be.true();
+                tfClose.should.be.false();
+            });
+        });
+
+        describe('IsOpen', () => {
+            const s = new uart.UartSocket({port:port});
+            it('should return \'false\' if Open has not been called', () => {
+                const tf = s.IsOpen;
+                tf.should.be.false();
+            });
+        });
+
+        describe('Close', () => {
+            it('should execute without error if port is already closed', async () => {
+                const s = new uart.UartSocket({port:port});
+                s.IsOpen.should.be.false();
+
+                await s.Close();
+
+    
+            });
+        });
+
+        describe('SendReceive', () => {
+            it('should respond to echo request', async () => {
+                const s = new uart.UartSocket({port:port});
+                await s.Open();
+                const message = `{"hello":"world"}`;
+                const request = buildEchoRequest(message);
+        
+                const response = await s.SendReceive(request);
+        
+                await s.Close();
+                response.should.containEql(message)
+                
+    
+            });
+
+            it('should throw an error if socket is not open', () => {
+                const s = new uart.UartSocket({port:port});
+                s.IsOpen.should.be.false();
+
+                return (s.SendReceive(`\n`)).should.be.rejectedWith({ message: 'Socket not open' });
+            });
+
+            it('should be able to send and receive large request body', async () => {
+                const s = new uart.UartSocket({port:port});
+                await s.Open();
+
+                const message = JSON.stringify(largeBody);
+                const request = buildEchoRequest(message);
+
+                try{
+                    const response = await s.SendReceive(request);
+                    response.should.containEql(message);
+                }
+                finally{
+                    await s.Close();
+                }
+            });
+
+        });
+
+        describe('Send', () => {
+            it('should execute without error', async () => {
+                const s = new uart.UartSocket({port:port});
+                await s.Open();
+                const command = buildEchoCommand(`{"random":"command}`);
+                
+                try{
+                    await s.Send(command);
+                }
+                finally{
+                    await s.Close();
+                }
+            });
+
+            it('should throw an error if socket is not open', () => {
+                const s = new uart.UartSocket({port:port});
+                s.IsOpen.should.be.false();
+
+                return (s.Send(`\n`)).should.be.rejectedWith({ message: 'Socket not open' });
+            });
+        });
+
+    });
+
+    describe('FindPort', () => {
+        it('should return empty array if no port can be found', async () => {
+            const params = {productId: '0043',
+                            vendorId: '2341'
+                            };
+            const p = await uart.FindPort(params);
+        
+            p.should.be.Array();
+            p.should.be.empty();
+
+        });
+
+        it('should return a port path if port can be found', async () => {
+            const params = {productId: '0001',
+                            vendorId: '30A4'
+                            };
+            const p = await uart.FindPort(params);
+
+            p.should.be.Array();
+            p[0].should.be.String();
+            p[0].should.not.be.empty();
+        });
+
+        it('should return all port paths with no search params', async () => {
+            const p = await uart.FindPort();
+            p.should.be.Array();
+            p.length.should.be.greaterThan(0);
+            for(let i = 0; i<p.length; i++){
+                p[i].should.be.String();
+            }
+        });
+
+        it('should return empty array if search parameter cannot be found', async () => {
+            const params = {invalidSearchParameter: 'random-value'
+                            };
+            const p = await uart.FindPort(params);
+
+            p.should.be.Array();
+            p.should.be.empty();
+
+        });
+
+        
+
+        
+    });
+});
+
+
+
+async function findNotecard() {
+    notecardUsbParams = {productId: '0001',
+                        vendorId: '30A4'
+                        }
+    const p = await uart.FindPort(notecardUsbParams);
+
+    return(p[0]);
+
+}
+
+function buildEchoRequest(body){
+    return `{"req":"echo","body":${body}}\n`;
+}
+
+function buildEchoCommand(body){
+    return `{"cmd":"echo","body":${body}}\n`;
+}
+
+
+
+const largeBody = {
+        longstring:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        glossary: {
+            title: "example glossary",
+            GlossDiv: {
+                title: "S",
+                GlossList: {
+                    GlossEntry: {
+                        ID: "SGML",
+                        SortAs: "SGML",
+                        GlossTerm: "Standard Generalized Markup Language",
+                        Acronym: "SGML",
+                        Abbrev: "ISO 8879:1986",
+                        GlossDef: {
+                            para: "A meta-markup language, used to create markup languages such as DocBook.",
+                            GlossSeeAlso: [
+                                "GML",
+                                "XML"
+                            ]
+                        },
+                        "GlossSee": "markup"
+                    }
+                }
+            }
+        }
+    }
