8e50d9671263828188572d319b515cd2b6e7809c
Greg Wolff <gwolff@blues.com>
2021-05-15 14:14:13 -0400

    Adds test for hammering Notecard with requests

---
+++ test/test_hammer.js
@@ -0,0 +1,61 @@
+const notecard = require('../notecard/notecard.js');
+
+
+const isEnabled = true;
+const connection = 'uart';
+//const connection = 'i2c';
+
+let numIterations = 25;
+
+
+
+(isEnabled ? describe : describe.skip)('Hammer Notecard with Requests', () => {
+
+    const request = {req:"card.version"};
+    const nc = new notecard.Notecard();
+    
+    it('should survive being hammered by requests', async () => {
+        nc.Socket = await getSocket(connection);
+        
+        await nc.Connect();
+        try{
+            for(var i = 0; i< numIterations; i++){
+                
+                const response = await nc.SendRequest(request);
+                response.should.not.be.empty();
+            }
+        }finally{
+            await nc.Disconnect();
+        }
+    }).timeout(10000);
+    
+
+});
+
+async function getSocket(socketType){
+    if(socketType === 'uart'){
+        const s = require('../notecard/uart-socket');
+        const port = await findNotecard(s)
+        return new s.UartSocket({port:port});
+        
+        
+    }else if(socketType === 'i2c'){
+        const s = require('../notecard/i2c-socket');
+        return new s.I2CSocket()
+    }else{
+        throw new Error('Socket type not recognized')
+    }
+}
+
+
+
+
+async function findNotecard(s) {
+    notecardUsbParams = {productId: '0001',
+                        vendorId: '30A4'
+                        }
+    const p = await s.FindPort(notecardUsbParams);
+
+    return(p[0]);
+
+}
