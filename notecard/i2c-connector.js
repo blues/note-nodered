f211fb2c0702bcf0ae1f82b6d5ab003b82ba9972
Greg Wolff <gwolff@blues.com>
2021-06-02 19:46:54 +0100

    Adds retry feature when receving bad response on i2c

--- notecard/i2c-connector.js
+++ notecard/i2c-connector.js
@@ -16,7 +16,7 @@ const I2C = getI2CInterface();
 const protocol = require('./bus-serial-protocol');
 const RESPONSE_BYTE_POLL_INTERVAL_MS = 5;
 const RESPONSE_BYTE_WAIT_INTERVAL_MS = 20;
-const WRITE_CHUNK_WAIT_INTERVAL_MS = 5;
+const WRITE_CHUNK_WAIT_INTERVAL_MS = 250;
 const MESSAGE_TERMINATOR = 0x0a;
 
 class I2CConnector{
@@ -65,36 +65,44 @@ class I2CConnector{
     async SendReceive(data){
         if(!this.IsOpen)
             throw new Error('Connector not open');
-
         const r = (n) => this.read(n);
         const w = (b) => this.write(b);
         const delay = () => new Promise(resolve => {setTimeout(()=>resolve(), WRITE_CHUNK_WAIT_INTERVAL_MS)});
         
-        await protocol.SendByteChunks(w, Buffer.from(data), delay);
-        
-        await timeout(RESPONSE_BYTE_WAIT_INTERVAL_MS);
-        const numBytes = await this.responseBytesAreAvailable();
-
-        let continueReceiving = true;
-        const receiveTimeoutMs = 5000;
+        const receiveTimeoutMs = 1000;
         const t = setTimeout(() => {
             continueReceiving = false;
             throw new Error("Timeout reading response from I2C bus");
         }, receiveTimeoutMs);
 
-        const RESPONSE_READ_POLL_INTERVAL_MS = 2;
-        let response = Buffer.alloc(0);
-        while(continueReceiving){
+        const doRetry = true;
+        while(doRetry){
+            await protocol.SendByteChunks(w, Buffer.from(data), delay);
+            
+            await timeout(RESPONSE_BYTE_WAIT_INTERVAL_MS);
+            const numBytes = await this.responseBytesAreAvailable();
 
-            const c = await protocol.ReceiveByteChunks(r, w, numBytes);
-            response = Buffer.concat([response, c]);
-            const hasTerminator = c[c.length-1] === MESSAGE_TERMINATOR;
-            if(hasTerminator){
-                clearTimeout(t);
-                return response.toString();
+            let continueReceiving = true;
+            
+
+            const RESPONSE_READ_POLL_INTERVAL_MS = 50;
+            let response = Buffer.alloc(0);
+            await timeout(1);
+            while(continueReceiving){
+
+                const c = await protocol.ReceiveByteChunks(r, w, numBytes);
+                response = Buffer.concat([response, c]);
+
+                const hasTerminator = Buffer.compare(c.slice(-2), Buffer.from([0x0d,0x0a])) === 0;
+                if(hasTerminator){
+                    clearTimeout(t);
+                    return response.toString();
+                }
+                if(Buffer.compare(c.slice(-2), Buffer.from([0x0a,0x0a])) === 0)
+                    break;
+
+                await timeout(RESPONSE_READ_POLL_INTERVAL_MS);
             }
-
-            await timeout(RESPONSE_READ_POLL_INTERVAL_MS);
         }
 
         
